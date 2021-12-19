f1a4510d1f5174d36cc33831e31ea953e446658e
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2021-12-19 14:36:44 +0100

    installing angular cli

--- pipeline/tasks/test-demo-app/task.sh
+++ pipeline/tasks/test-demo-app/task.sh
@@ -8,6 +8,9 @@ cd repository/demo-app
 echo "Installing npm dependencies"
 npm install
 
+echo "Installing Angular CLI"
+npm install -g @angular/cli
+
 echo "Running Angular tests"
 ng test --watch=false --browser=ChromeHeadless
 
